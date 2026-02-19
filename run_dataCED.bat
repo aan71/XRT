609093096fa371c8e1395c8685ef3972caa182e3
aan71 <57895043+aan71@users.noreply.github.com>
2026-02-19 09:00:17 +0100

    Delete run_dataCED.bat

--- run_dataCED.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Batch file to run dataCED.py using the virtual environment Python
-
-REM Get the directory where this batch file is located
-set SCRIPT_DIR=%~dp0
-
-REM Run dataCED.py using the virtual environment Python
-"%SCRIPT_DIR%.venv\Scripts\python.exe" "%SCRIPT_DIR%dataCED.py" %*
-
